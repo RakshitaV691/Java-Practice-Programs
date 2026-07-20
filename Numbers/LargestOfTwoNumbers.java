45b9c3b38dc15b6a75206bbb1844754ddbbc8cef
RakshitaV691 <rakshitav246@gmail.com>
2026-07-20 14:22:05 +0530

    Add LargestOfTwoNumbers.java

---
+++ Numbers/LargestOfTwoNumbers.java
@@ -0,0 +1,15 @@
+public class LargestOfTwoNumbers
+{
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 100;
+        if(a>b)
+            System.out.println(a + " is Largest");
+        else
+            System.out.println(b + " is Largest");
+
+            
+    }
+
+
+}
