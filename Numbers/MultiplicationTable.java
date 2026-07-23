bf344fb94493498a505a04eafdbc55d0b94528c8
RakshitaV691 <rakshitav246@gmail.com>
2026-07-23 13:31:29 +0530

    Add MultiplicationTable.java

---
+++ Numbers/MultiplicationTable.java
@@ -0,0 +1,11 @@
+public class MultiplicationTable 
+{
+    public static void main(String[] args) {
+        int a = 4;
+    for(int i=1; i<=10; i++){
+        System.out.println(a + " X " + i + " = " + a*i);
+    }
+    }
+    
+    
+}
